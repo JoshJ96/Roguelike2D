6709f32501d528b282ca12fb0511e348dda05137
Joshua Jaworowski <joshuajaworowski@gmail.com>
2020-05-17 23:46:45 -0400

    Organization & initial dungeon instantiation working

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    public int[,] map;
+    public int roomWidth;
+    public int roomHeight;
+    public GameObject wallObject;
+
+    private void Start()
+    {
+        //Fill map with walls
+        FillMap(roomWidth, roomHeight);
+
+        //Print map tiles
+        PrintMap(map);
+    }
+
+    //Fill map with walls based on width and height
+    private void FillMap(int _roomWidth, int _roomHeight)
+    {
+        //Initialize map variable
+        map = new int[_roomWidth,_roomHeight];
+
+        //Loop through map array
+        for (int x = 0; x < roomWidth; x++)
+        {
+            for (int y = 0; y < roomHeight; y++)
+            {
+                map[x, y] = 1;
+            }
+        }
+    }
+
+    //Prints map with all tiles needed
+    private void PrintMap(int[,] map)
+    {
+        //Loop through map array
+        for (int x = 0; x < roomWidth; x++)
+        {
+            for (int y = 0; y < roomHeight; y++)
+            {
+                if (map[x, y] == 1)
+                {
+                    Instantiate(wallObject, new Vector2(x, y), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
